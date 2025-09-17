6d98fb6b590fd9931acc37d9ecfb98c6c9641314
martinIvovIv <martinivoviv@gmail.com>
2025-09-18 00:17:51 +0200

    feat: adds routing and secret admin page

---
+++ src/SecretAdminPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SecretAdminPage = () => {
+
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-900 to-blue-700 relative">
+        hello
+    </div>
+  );
+};
+
+export default SecretAdminPage;
